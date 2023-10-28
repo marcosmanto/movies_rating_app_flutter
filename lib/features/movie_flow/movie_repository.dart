c1b9141645d72e4c694bcc5a68a686ed5360ee9b
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-10-28 12:51:13 -0300

    Created MovieRepository that interact with database and create data entities objects to feed the application.

---
+++ lib/features/movie_flow/movie_repository.dart
@@ -0,0 +1,62 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:movies_rating_app_flutter/core/environment_variables.dart';
+import 'package:movies_rating_app_flutter/features/movie_flow/genre/genre_entity.dart';
+import 'package:movies_rating_app_flutter/features/movie_flow/result/movie_entity.dart';
+import 'package:movies_rating_app_flutter/main.dart';
+
+final movieRepositoryProvider = Provider<MovieRepository>((ref) {
+  final dio = ref.read(dioProvider);
+  return TMDBMovieRepository(dio: dio);
+});
+
+abstract class MovieRepository {
+  Future<List<GenreEntity>> getMoviesGenres();
+  Future<List<MovieEntity>> getRecommendedMovies(
+      double rating, String date, String genreIds);
+}
+
+class TMDBMovieRepository implements MovieRepository {
+  final Dio dio;
+
+  TMDBMovieRepository({required this.dio});
+
+  @override
+  Future<List<GenreEntity>> getMoviesGenres() async {
+    final response = await dio.get(
+      'genre/movie/list',
+      queryParameters: {
+        'api_key': api,
+        'language': 'en-US',
+      },
+    );
+
+    final results = List<Map<String, dynamic>>.from(response.data['genres']);
+    final genres = results.map((e) => GenreEntity.fromMap(e)).toList();
+    return genres;
+  }
+
+  @override
+  Future<List<MovieEntity>> getRecommendedMovies(
+      double rating, String date, String genreIds) async {
+    final response = await dio.get(
+      'discover/movie',
+      queryParameters: {
+        'api_key': api,
+        'language': 'en-US',
+        'sort_by': 'popularity.desc',
+        'include_adult': false,
+        'vote_average.gte': rating,
+        'page': 1,
+        'release_date.gte': date,
+        'with_genres': genreIds,
+      },
+    );
+
+    final results = List<Map<String, dynamic>>.from(response.data['results']);
+    final List<MovieEntity> movies =
+        results.map((e) => MovieEntity.fromMap(e)).toList();
+
+    return movies;
+  }
+}
