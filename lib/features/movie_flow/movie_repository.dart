fe1bd0186fca6117b3f54c21bc91ff12d3182d7f
Marcos Mantovani <mantovani.marcos@gmail.com>
2023-10-28 23:26:31 -0300

    Add custom failure class and try catchs in MovieRepository to handle Dio connection errors.

--- lib/features/movie_flow/movie_repository.dart
+++ lib/features/movie_flow/movie_repository.dart
@@ -1,12 +1,15 @@
+import 'dart:io';
+
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:movies_rating_app_flutter/core/environment_variables.dart';
+import 'package:movies_rating_app_flutter/core/failure.dart';
 import 'package:movies_rating_app_flutter/features/movie_flow/genre/genre_entity.dart';
 import 'package:movies_rating_app_flutter/features/movie_flow/result/movie_entity.dart';
 import 'package:movies_rating_app_flutter/main.dart';
 
 final movieRepositoryProvider = Provider<MovieRepository>((ref) {
-  final dio = ref.read(dioProvider);
+  final dio = ref.watch(dioProvider);
   return TMDBMovieRepository(dio: dio);
 });
 
@@ -23,41 +26,63 @@ class TMDBMovieRepository implements MovieRepository {
 
   @override
   Future<List<GenreEntity>> getMoviesGenres() async {
-    final response = await dio.get(
-      'genre/movie/list',
-      queryParameters: {
-        'api_key': api,
-        'language': 'en-US',
-      },
-    );
+    try {
+      final response = await dio.get(
+        'genre/movie/list',
+        queryParameters: {
+          'api_key': api,
+          'language': 'en-US',
+        },
+      );
 
-    final results = List<Map<String, dynamic>>.from(response.data['genres']);
-    final genres = results.map((e) => GenreEntity.fromMap(e)).toList();
-    return genres;
+      final results = List<Map<String, dynamic>>.from(response.data['genres']);
+      final genres = results.map((e) => GenreEntity.fromMap(e)).toList();
+      return genres;
+    } on DioException catch (e) {
+      if (e.error is SocketException) {
+        throw Failure(message: 'No internet connection', exception: e);
+      }
+
+      throw Failure(
+        message: e.response?.statusMessage ?? 'Something went wrong',
+        code: e.response?.statusCode,
+      );
+    }
   }
 
   @override
   Future<List<MovieEntity>> getRecommendedMovies(
       double rating, String date, String genreIds) async {
-    final response = await dio.get(
-      'discover/movie',
-      queryParameters: {
-        'api_key': api,
-        'language': 'en-US',
-        'sort_by': 'popularity.desc',
-        'include_adult': false,
-        'vote_average.gte': rating,
-        'page': 1,
-        'primary_release_year': date.substring(0, 4),
-        //'release_date.lte': date,
-        'with_genres': genreIds,
-      },
-    );
+    try {
+      final response = await dio.get(
+        'discover/movie',
+        queryParameters: {
+          'api_key': api,
+          'language': 'en-US',
+          'sort_by': 'popularity.desc',
+          'include_adult': false,
+          'vote_average.gte': rating,
+          'page': 1,
+          'primary_release_year': date.substring(0, 4),
+          //'release_date.lte': date,
+          'with_genres': genreIds,
+        },
+      );
 
-    final results = List<Map<String, dynamic>>.from(response.data['results']);
-    final List<MovieEntity> movies =
-        results.map((e) => MovieEntity.fromMap(e)).toList();
+      final results = List<Map<String, dynamic>>.from(response.data['results']);
+      final List<MovieEntity> movies =
+          results.map((e) => MovieEntity.fromMap(e)).toList();
 
-    return movies;
+      return movies;
+    } on DioException catch (e) {
+      if (e.error is SocketException) {
+        throw Failure(message: 'No internet connection', exception: e);
+      }
+
+      throw Failure(
+        message: e.response?.statusMessage ?? 'Something went wrong',
+        code: e.response?.statusCode,
+      );
+    }
   }
 }
